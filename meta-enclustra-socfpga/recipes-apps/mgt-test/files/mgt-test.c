79e14414b9fd918deee998766ab6872bd1225b46
Andreas Buerkler <andreas.buerkler@enclustra.com>
2023-06-15 19:33:30 +0200

    MGT test updated

--- meta-enclustra-socfpga/recipes-apps/mgt-test/files/mgt-test.c
+++ meta-enclustra-socfpga/recipes-apps/mgt-test/files/mgt-test.c
@@ -5,45 +5,48 @@
 #include <unistd.h>
 #include <stdint.h>
 #include <time.h>
+#include <math.h>
 
 #define MGT_BASE    0xFF280000
+#define PLL_BASE    0xff290000
 #define MGT_LEN     0x1000
-#define VERSION     "0.2"
+#define PLL_LEN     0x1000
+#define REFCLK_MHZ  125
+#define VERSION     "0.3"
 
-uint32_t* mem = NULL;
-off_t page_offset = 0;
+#define debug_print(...) printf("\x1b[0m" __VA_ARGS__)
+#define info_print(...) printf("\x1b[33m" __VA_ARGS__)
 
-uint32_t mem_read(uint32_t offset)
+uint32_t* mgt_mem = NULL;
+uint32_t* pll_mem = NULL;
+
+uint32_t mem_read(uint32_t* mem, uint32_t offset)
 {
-    uint32_t addr = (page_offset + offset) / sizeof(uint32_t);
+    uint32_t addr = offset / sizeof(uint32_t);
     uint32_t retval = mem[addr];
-    printf("    Read  0x%08x from 0x%08x\r\n", retval, addr);
+    debug_print("    Read  0x%08x from 0x%08x\r\n", retval, addr);
     return retval;
 }
 
-void mem_write(uint32_t offset, uint32_t value)
+void mem_write(uint32_t* mem, uint32_t offset, uint32_t value)
 {
-    uint32_t addr = (page_offset + offset) / sizeof(uint32_t);
-    printf("    Write 0x%08x  to  0x%08x\r\n", value, addr);
+    uint32_t addr = offset / sizeof(uint32_t);
+    debug_print("    Write 0x%08x  to  0x%08x\r\n", value, addr);
     mem[addr] = value;
 }
 
-void mem_modify(uint32_t offset, uint32_t value, uint32_t mask)
+void mem_modify(uint32_t* mem, uint32_t offset, uint32_t value, uint32_t mask)
 {
-    uint32_t val = mem_read(offset);
+    uint32_t val = mem_read(mem, offset);
     val = (val & ~mask) | (value & mask);
-    mem_write(offset, val);
+    mem_write(mem, offset, val);
 }
 
-int mem_init()
+int mem_init(uint32_t base, uint32_t len, uint32_t** mem)
 {
-    size_t pagesize = sysconf(_SC_PAGE_SIZE);
-    off_t page_base = (MGT_BASE / pagesize) * pagesize;
-    page_offset = MGT_BASE - page_base;
-
     int fd = open("/dev/mem", O_RDWR | O_SYNC);
-    mem = (uint32_t*)mmap(NULL, page_offset + MGT_LEN, PROT_READ | PROT_WRITE, MAP_SHARED, fd, page_base);
-    if (mem == MAP_FAILED) {
+    *mem = (uint32_t*)mmap(NULL, len, PROT_READ | PROT_WRITE, MAP_SHARED, fd, base);
+    if (*mem == MAP_FAILED) {
         perror("Can't map memory");
         return -1;
     }
@@ -62,49 +65,49 @@ uint64_t get_time_ms()
 
 int mgt_test()
 {
-    printf("MGT test version %s\r\n", VERSION);
+    info_print("MGT test version %s\r\n", VERSION);
 
-    // Check access
-    mem_read(0x200 * 4);
-    mem_read(0x204 * 4);
-    mem_read(0x205 * 4);
-    mem_read(0x210 * 4);
-    mem_read(0x211 * 4);
-    mem_read(0x212 * 4);
+    // Check if fPLL is locked
+    uint32_t lock = mem_read(pll_mem, 0x280 * 4) & 0x01;
+    info_print("fPLL lock status: %i\r\n", lock);
 
-    // Request bus access
-
+    // Print data rate
+    uint32_t pll_m = ((mem_read(pll_mem, 0x12c * 4) & 0x01) << 8) | mem_read(pll_mem, 0x12b * 4);
+    uint32_t pll_n = (mem_read(pll_mem, 0x12c * 4) >> 3) & 0x1f;
+    uint32_t pll_l = (uint32_t)powf(2.0, (float)((mem_read(pll_mem, 0x12c * 4) >> 1) & 0x03));
+    uint32_t data_rate = 2 * REFCLK_MHZ / pll_n * pll_m / pll_l;
+    info_print("Data rate: %i Mbps\r\n", data_rate);
 
     // Enable reset
-    printf("Enable reset\r\n");
-    mem_write(0x2e2 * 4, 0xf0); // get software control of reset
-    mem_write(0x2e2 * 4, 0xfa); // enable digital reset
-    mem_write(0x2e2 * 4, 0xff); // enable analog reset
+    debug_print("Enable reset\r\n");
+    mem_write(mgt_mem, 0x2e2 * 4, 0xf0); // get software control of reset
+    mem_write(mgt_mem, 0x2e2 * 4, 0xfa); // enable digital reset
+    mem_write(mgt_mem, 0x2e2 * 4, 0xff); // enable analog reset
 
     // Enable PRBS 31 generator in 64bit mode
-    printf("Enable PRBS generator\r\n");
-    mem_modify(0x006 * 4, 0x44, 0xcf);
-    mem_modify(0x007 * 4, 0x00, 0xf0);
-    mem_modify(0x008 * 4, 0x10, 0x70);
-    mem_modify(0x110 * 4, 0x03, 0x07);
+    debug_print("Enable PRBS generator\r\n");
+    mem_modify(mgt_mem, 0x006 * 4, 0x44, 0xcf);
+    mem_modify(mgt_mem, 0x007 * 4, 0x00, 0xf0);
+    mem_modify(mgt_mem, 0x008 * 4, 0x10, 0x70);
+    mem_modify(mgt_mem, 0x110 * 4, 0x03, 0x07);
 
     // Enable PRBS 31 checker in 64bit mode
-    printf("Enable PRBS checker\r\n");
-    mem_modify(0x00a * 4, 0x80, 0x80);
-    mem_modify(0x00b * 4, 0x0c, 0xfc);
-    mem_modify(0x00c * 4, 0x01, 0x01);
-    mem_modify(0x13f * 4, 0x0e, 0x0f);
+    debug_print("Enable PRBS checker\r\n");
+    mem_modify(mgt_mem, 0x00a * 4, 0x80, 0x80);
+    mem_modify(mgt_mem, 0x00b * 4, 0x0c, 0xfc);
+    mem_modify(mgt_mem, 0x00c * 4, 0x01, 0x01);
+    mem_modify(mgt_mem, 0x13f * 4, 0x0e, 0x0f);
 
     // Release reset
-    printf("Release reset\r\n");
-    mem_write(0x2e2 * 4, 0xfa);
-    mem_write(0x2e2 * 4, 0xf0);
+    debug_print("Release reset\r\n");
+    mem_write(mgt_mem, 0x2e2 * 4, 0xfa);
+    mem_write(mgt_mem, 0x2e2 * 4, 0xf0);
 
     // Enable PRBS counter
-    printf("Enable PRBS counter\r\n");
-    mem_modify(0x300 * 4, 0x01, 0x01); // enable counter
-    mem_modify(0x300 * 4, 0x02, 0x02); // reset
-    mem_modify(0x300 * 4, 0x00, 0x02); // clear reset
+    debug_print("Enable PRBS counter\r\n");
+    mem_modify(mgt_mem, 0x300 * 4, 0x01, 0x01); // enable counter
+    mem_modify(mgt_mem, 0x300 * 4, 0x02, 0x02); // reset
+    mem_modify(mgt_mem, 0x300 * 4, 0x00, 0x02); // clear reset
 
     uint64_t start_time_ms = get_time_ms();
 
@@ -112,51 +115,55 @@ int mgt_test()
     sleep(10);
 
     // Capture snapshot of counter registers
-    mem_modify(0x300 * 4, 0x04, 0x04);
+    mem_modify(mgt_mem, 0x300 * 4, 0x04, 0x04);
     uint64_t end_time_ms = get_time_ms();
 
     uint64_t error_counter = 0;
-    error_counter |= (uint64_t)mem_read(0x301 * 4);
-    error_counter |= (uint64_t)mem_read(0x302 * 4) << 8;
-    error_counter |= (uint64_t)mem_read(0x303 * 4) << 16;
-    error_counter |= (uint64_t)mem_read(0x304 * 4) << 24;
-    error_counter |= (uint64_t)mem_read(0x305 * 4) << 32;
-    error_counter |= (uint64_t)mem_read(0x306 * 4) << 40;
-    error_counter |= (uint64_t)mem_read(0x307 * 4) << 48;
+    error_counter |= (uint64_t)mem_read(mgt_mem, 0x301 * 4);
+    error_counter |= (uint64_t)mem_read(mgt_mem, 0x302 * 4) << 8;
+    error_counter |= (uint64_t)mem_read(mgt_mem, 0x303 * 4) << 16;
+    error_counter |= (uint64_t)mem_read(mgt_mem, 0x304 * 4) << 24;
+    error_counter |= (uint64_t)mem_read(mgt_mem, 0x305 * 4) << 32;
+    error_counter |= (uint64_t)mem_read(mgt_mem, 0x306 * 4) << 40;
+    error_counter |= (uint64_t)mem_read(mgt_mem, 0x307 * 4) << 48;
 
     uint64_t bit_counter = 0;
-    bit_counter |= (uint64_t)mem_read(0x30d * 4);
-    bit_counter |= (uint64_t)mem_read(0x30e * 4) << 8;
-    bit_counter |= (uint64_t)mem_read(0x30f * 4) << 16;
-    bit_counter |= (uint64_t)mem_read(0x310 * 4) << 24;
-    bit_counter |= (uint64_t)mem_read(0x311 * 4) << 32;
-    bit_counter |= (uint64_t)mem_read(0x312 * 4) << 40;
-    bit_counter |= (uint64_t)mem_read(0x313 * 4) << 48;
+    bit_counter |= (uint64_t)mem_read(mgt_mem, 0x30d * 4);
+    bit_counter |= (uint64_t)mem_read(mgt_mem, 0x30e * 4) << 8;
+    bit_counter |= (uint64_t)mem_read(mgt_mem, 0x30f * 4) << 16;
+    bit_counter |= (uint64_t)mem_read(mgt_mem, 0x310 * 4) << 24;
+    bit_counter |= (uint64_t)mem_read(mgt_mem, 0x311 * 4) << 32;
+    bit_counter |= (uint64_t)mem_read(mgt_mem, 0x312 * 4) << 40;
+    bit_counter |= (uint64_t)mem_read(mgt_mem, 0x313 * 4) << 48;
 
-    // check if channel is locked (rx_is_lockedtodata)
-    uint32_t locked_to_data = mem_read(0x280 * 4) & 0x01;
-    uint32_t locked_to_ref = (mem_read(0x280 * 4) & 0x02) >> 1;
+    // Check if channel is locked (rx_is_lockedtodata)
+    uint32_t locked_to_data = mem_read(mgt_mem, 0x280 * 4) & 0x01;
+    uint32_t locked_to_ref = (mem_read(mgt_mem, 0x280 * 4) & 0x02) >> 1;
 
-    // calculate BER and data rate
+    // Calculate BER and data rate
     double bit_double = (double)bit_counter;
     double error_double = (double)error_counter;
     double running_time_ms = (double)(end_time_ms - start_time_ms);
-
-    double rate = ((bit_double / running_time_ms) / 1000) * 31; // * 31 because PRBS31
     double ber = (error_double + 1.0) / bit_double;
 
-    printf("status after %.1f seconds:\r\n", running_time_ms/1000);
-    printf("locked: clock %i, data %i\r\n", locked_to_ref, locked_to_data);
-    printf("bits: %.0f, errors: %.0f\r\n", bit_double, error_double);
-    printf("rate: %.0f Mbps, ber: %f\r\n", rate, ber);
+    info_print("Status after %.1f seconds:\r\n", running_time_ms/1000);
+    info_print("Locked: clock %i, data %i\r\n", locked_to_ref, locked_to_data);
+    info_print("Bits: %.0f, errors: %.0f\r\n", bit_double, error_double);
+    info_print("BER: %.3e\r\n", ber);
 
+    debug_print("Exit\r\n");
     return 0;
 }
 
 int main(int argc, char *argv[])
 {
     int ret = 0;
-    ret = mem_init();
+    ret = mem_init(MGT_BASE, MGT_LEN, &mgt_mem);
+    if (ret)
+    {
+        return ret;
+    }
+    ret = mem_init(PLL_BASE, PLL_LEN, &pll_mem);
     if (ret)
     {
         return ret;
